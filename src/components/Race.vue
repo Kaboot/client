666a02db8a9ec4733b76c0ba8ccde472a3afb7da
afifahrahmak <afifahrk96@gmail.com>
2019-11-14 21:42:01 +0700

    done welcoming page +start+countdown

---
+++ src/components/Race.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="race">
+    <h1>This is Racing Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Race"
+};
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Main from '../views/Main.vue'
+
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/race',
+    name: 'main',
+    component: Main
   }
 ]
 
