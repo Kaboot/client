8ea70631c25b28b572bc444db4555809aeef66bd
afifahrahmak <afifahrk96@gmail.com>
2019-11-15 11:19:45 +0700

    coba

--- src/components/Race.vue
+++ src/components/Race.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="race">
-    <h1>NGUEEENGG!!</h1>
+    <h1>{{mess}}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Race"
+  name: "Race",
+  data() {
+    return {
+      messages: [
+        "NGUEENG!!",
+        "WHOOOZZ!",
+        "VROOM VROOMM!",
+        "CKIIITTT!",
+        "DUAAARR!"
+      ],
+      mess: ""
+    };
+  },
+  methods: {
+    showmess() {
+      this.messages.forEach(message => {
+        this.mess = message;
+        setTimeout(function() {
+          return this.mess;
+        }, 3000);
+      });
+    }
+  },
+  created() {
+    this.showmess();
+  },
+  computed() {
+    this.showmess();
+  }
 };
 </script>
 
@@ -14,8 +42,10 @@ export default {
 @import url("https://fonts.googleapis.com/css?family=Caveat+Brush|Covered+By+Your+Grace|Gloria+Hallelujah|Holtwood+One+SC|Patrick+Hand+SC&display=swap");
 h1 {
   font-family: "Patrick Hand SC";
-  color: antiquewhite;
+  color: rgb(35, 35, 33);
   font-weight: bolder;
+  background-color: rgba(250, 235, 215, 0.571);
+  width: auto;
 }
 #race {
   background-image: url("../assets/Space-Phone-Wallpaper.jpg");
